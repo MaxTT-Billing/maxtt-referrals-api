a78205bef552b433bca803aed51edb24436a920d
MaxTT-Billing <treadstonesolutions2023@gmail.com>
2025-08-22 18:58:28 +0530

    Update 002_seed_keys.sql

--- sql/002_seed_keys.sql
+++ sql/002_seed_keys.sql
@@ -1,4 +1,3 @@
--- Optional one-time seeds; runtime will overwrite with fresh hashes via ensureKeyHashes()
 insert into api_keys (name, key_hash, role)
 values
   ('billing-writer', '$2a$10$placeholderplaceholderplaceholderpl', 'writer'),
